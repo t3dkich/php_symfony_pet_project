5fd2911cd2f24e4081a4ae04c5f7dbd8b456b0ef
t3dkich <t3dkich@gmail.com>
2019-01-12 19:44:26 +0200

    test

--- src/AppBundle/Service/UserService.php
+++ src/AppBundle/Service/UserService.php
@@ -92,17 +92,20 @@ class UserService implements UserServiceInterface
 
     public function details(UserDetails $userDetails)
     {
-        $details = $this->entityManager->getRepository(UserDetails::class)
-            ->findOneBy(['user' => $this->security->getUser()]);
+//        $details = $this->entityManager->getRepository(UserDetails::class)
+//            ->findOneBy(['user' => $this->security->getUser()]);
+//
+//
+//        if (null === $details) {
+//            $this->entityManager->persist($userDetails);
+//            $this->entityManager->flush();
+//        } else {
+//            $this->entityManager->merge($userDetails->setId($details->getId()));
+//            $this->entityManager->flush();
+//        }
 
-
-        if (null === $details) {
-            $this->entityManager->persist($userDetails);
-            $this->entityManager->flush();
-        } else {
-            $this->entityManager->merge($userDetails->setId($details->getId()));
-            $this->entityManager->flush();
-        }
+        $this->entityManager->persist($userDetails);
+        $this->entityManager->flush();
     }
 
     public function getAll()
